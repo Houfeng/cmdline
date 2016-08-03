8cfc161b3035c9c369185547578f42a6f8256f2f
Houfeng <admin@xhou.net>
2016-08-03 17:31:41 +0800

    1.0..1

--- bin/cli.js
+++ bin/cli.js
@@ -6,10 +6,10 @@ const pkg = require('../package.json');
 cmdline
   .version(pkg.version)
   .help('info')
-  .command('start', function (cmd, $0) {
-    console.log('cmd:', cmd, $0);
-  }, true)
-  .handler({ argc: 1 }, function ($0) {
+  // .command('start', function (cmd, $0) {
+  //   console.log('cmd:', cmd, $0);
+  // }, true)
+  .handler({ arguments: true }, function ($0, $1) {
     console.log('argv:', $0);
   })
   .handler(function () {
