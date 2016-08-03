9efe560125b91d422cf6c112448b22093a6a30d2
Houfeng <admin@xhou.net>
2016-08-03 21:39:27 +0800

    dev

--- bin/cli.js
+++ bin/cli.js
@@ -6,6 +6,10 @@ const pkg = require('../package.json');
 cmdline
   .version(pkg.version)
   .help('info')
+  .error(function (err) {
+    console.error('ERR:', err.message);
+    process.exit(1);
+  })
   .option('-t', { type: 'switch' })
   .command('start', function (cmd, t) {
     console.log('cmd:', t);
