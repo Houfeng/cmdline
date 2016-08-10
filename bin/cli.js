b07621fb4ade82a529a6d9e85ea7cd6a13b5a2a3
Houfeng <admin@xhou.net>
2016-08-10 21:39:25 +0800

    1.1.5

--- bin/cli.js
+++ bin/cli.js
@@ -5,13 +5,14 @@ const pkg = require('../package.json');
 
 cmdline
   .version(pkg.version)
-  .help('info')
+  .help('cmdline help')
   .error(function (err) {
     console.error(err.message);
     process.exit(1);
   })
   .option('-t', 'switch')
   .command('start')
+  .help('start help')
   .root.action(function ($1, $2) {
     console.log('argv:', $1);
     //return false;
